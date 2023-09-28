fd2c6c7e21616b5cef9fa7b0a555edcee5704375
mrcut <xxmrcutxx@gmail.com>
2023-09-28 10:12:19 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './core/components/layout/NavBar';
-import MainRoutes from './core/components/routes/MainRoutes';
-import {BrowserRouter} from 'react-router-dom';
+import "./App.css";
+import NavBar from "./core/components/layout/NavBar";
+import MainRoutes from "./core/components/routes/MainRoutes";
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./core/components/layout/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar/>
-        <MainRoutes/>
+        <NavBar />
+        <MainRoutes />
+        <Footer />
       </BrowserRouter>
     </>
   );
