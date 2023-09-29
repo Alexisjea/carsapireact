f876da8b2f7c18c7b2378d55a1d4a7bbf17a33b1
UmanAJ <alexis.jeandenans@uman-control.com>
2023-09-29 09:53:10 +0200

    i18n : ok

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import NavBar from "./core/components/layout/NavBar";
 import MainRoutes from "./core/components/routes/MainRoutes";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./core/components/layout/Footer";
-
+import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import { UserContext } from "./core/components/contexts/UserContext";
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   const [user, setUser] = useState(JSON.parse(sessionStorage.getItem("USER")));
   return (
     <div>
